49b914b87176555a26ac0675833a1c93e35779cd
camachoc915 <89162574+camachoc915@users.noreply.github.com>
2023-03-11 13:35:07 -0800

    Weather App Transfer
    Transferred current weather app project folder to Github

---
+++ src/index.js
@@ -0,0 +1,60 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+  return `${day} ${hours}:${minutes}`;
+}
+function displayWeather(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+function searchLocation(position) {
+  let apiKey = "35b7b3764c67d150d7f0fed207b2041b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "35b7b3764c67d150d7f0fed207b2041b";
+  let city = document.querySelector("#city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+let cityElement = document.querySelector("#city");
+let searchForm = document.querySelector("#search-form");
+
+searchForm.addEventListener("submit", search);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+cityElement.innerHTML = "San Diego";
+dateElement.innerHTML = formatDate(currentTime);
