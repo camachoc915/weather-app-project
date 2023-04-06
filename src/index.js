a3cdeafcff7ba4399bf7cc477b8d32abd7469b06
camachoc915 <89162574+camachoc915@users.noreply.github.com>
2023-04-06 16:27:40 -0700

    Current Location fix
    Added axios function to fix the current location button.

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ function displayWeather(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "72bb9dab46b9ec3d65f423c63f27a9b8";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
@@ -67,6 +67,7 @@ function search(event) {
 function searchLocation(position) {
   let apiKey = "35b7b3764c67d150d7f0fed207b2041b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
 }
 
 function getCurrentLocation(event) {
